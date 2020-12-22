c5afe4092e50166e78ff24a2444ad55625304889
bob stubbs <bob.stubbs.hb@gmail.com>
2020-12-22 10:54:38 -0800

    initial check in - section 19

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { ManageOrdersComponent } from './manage-orders/manage-orders.component';
+import { LogoutComponent } from './logout/logout.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ManageProductsComponent } from './manage-products/manage-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'myorders', component: MyOrdersComponent},
+  {path: 'manageorders', component: ManageOrdersComponent},
+  {path: 'manageproducts', component: ManageProductsComponent},
+  {path: 'logout', component: LogoutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { ManageOrdersComponent } from './manage-orders/manage-orders.component';
+import { ManageProductsComponent } from './manage-products/manage-products.component';
+import { LogoutComponent } from './logout/logout.component';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MyOrdersComponent,
+    ManageOrdersComponent,
+    ManageProductsComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
